8b289e2539eb97fc46afa34ab7181ffad139cbb6
AntonPochernikov <akpochernikov@gmail.com>
2020-02-14 23:42:49 +0300

    Updates

---
+++ precache-manifest.d0246ab6c81f91b623bd938d50d9774d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67935e997589929a022c158d97c96fc5",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "cce112c420e5981d4d933fd2afc27b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec1b01b5a83ca0a60729",
+    "url": "/static/css/2.e4b6ee68.chunk.css"
+  },
+  {
+    "revision": "c377116628a6c3928a1e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ec1b01b5a83ca0a60729",
+    "url": "/static/js/2.236ed1e2.chunk.js"
+  },
+  {
+    "revision": "6c14fef57eff90a79c86",
+    "url": "/static/js/3.6b4857b9.chunk.js"
+  },
+  {
+    "revision": "c377116628a6c3928a1e",
+    "url": "/static/js/main.d305d959.chunk.js"
+  },
+  {
+    "revision": "87f7843d78e1e06717c6",
+    "url": "/static/js/runtime-main.ec4f3a95.js"
+  }
+]);
